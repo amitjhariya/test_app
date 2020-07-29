240ee3dcca5d7b892d73fe699d49f06ebe12c03b
Amit Jhariya <amit.jhariya93@gmail.com>
2020-07-30 02:45:08 +0530

    First commit

---
+++ precache-manifest.ed5408e9fc779923c1b2ea358a1adcb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b492316bbfbc3b5cc705db985eaa32",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "6e32c9cb6315a6bccf32",
+    "url": "/test_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d561a72fffa39fbb9684",
+    "url": "/test_app/static/js/2.2d00babf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_app/static/js/2.2d00babf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e32c9cb6315a6bccf32",
+    "url": "/test_app/static/js/main.630e7bd9.chunk.js"
+  },
+  {
+    "revision": "08f332e87f13ea5ab6a4",
+    "url": "/test_app/static/js/runtime-main.a8aa7b4e.js"
+  }
+]);
